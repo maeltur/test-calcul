2f429157c6534b56722f90af88967635632171eb
maeltur <61408835+maeltur@users.noreply.github.com>
2020-04-07 15:36:34 +0200

    Add files via upload

--- src/test/java/calculs/CalculsTests.java
+++ src/test/java/calculs/CalculsTests.java
@@ -4,10 +4,13 @@
 package calculs;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import static org.junit.jupiter.api.Assumptions.assumeFalse;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /**
  * 
@@ -15,6 +18,7 @@ import org.junit.jupiter.api.Test;
  * 
  */
 class CalculsTests {
+
 	private Calculs c1 = null;
 	/**
 	 * 
@@ -22,10 +26,46 @@ class CalculsTests {
 	 * 
 	 */
 	@BeforeEach
-	void setUp() throws Exception{
-		c1 = new Calculs(1,2);
-	}
-	
+    void setUp() throws Exception {
+    }        
+
+    @AfterEach
+    void tearDown() throws Exception {
+    }
+
+    static Stream<Arguments> chargerJeuDeTestMul() throws Throwable {
+            return Stream.of(
+                Arguments.of(2,2,4),
+                Arguments.of(6,6,36),
+                Arguments.of(3,2,6),
+                Arguments.of(3,3,3)
+            );
+    }
+    static Stream<Arguments> chargerJeuDeTestAdd() throws Throwable {
+        return Stream.of(
+            Arguments.of(2,2,4),
+            Arguments.of(6,6,12),
+            Arguments.of(3,2,5),
+            Arguments.of(3,3,3)
+        );
+}
+    static Stream<Arguments> chargerJeuDeTestDiv() throws Throwable {
+        return Stream.of(
+            Arguments.of(2,2,1),
+            Arguments.of(6,6,1),
+            Arguments.of(4,2,2),
+            Arguments.of(3,3,3)
+        );
+}
+    static Stream<Arguments> chargerJeuDeTestSou() throws Throwable {
+        return Stream.of(
+            Arguments.of(2,2,0),
+            Arguments.of(6,6,0),
+            Arguments.of(3,2,1),
+            Arguments.of(3,3,3)
+        );
+}
+
 	/**
 	 * Test method for {@link calculs.Calculs#Calculs(int, int)}.
 	 */
@@ -37,43 +77,42 @@ class CalculsTests {
 	/**
 	 * Test method for {@link calculs.Calculs#multiplier()}.
 	 */
-	@Test
-	void testMultiplier() {
-		assumeFalse(System.getProperty("os.name").contains("Windows"));
-		if (c1.multiplier()!=2){
-			fail("Erreur dans la multiplication");
-		}
-	}
+
+	@ParameterizedTest(name="Multiplication numéro {index}: nombre1={0}, nombre2={1}, résultat attendu = {2}")
+    @MethodSource("chargerJeuDeTestMul")
+    void testMultiplier(int firstNumber, int secondNumber, int expectedResult) {
+            Calculs monCal = new Calculs(firstNumber, secondNumber);
+            assertEquals(expectedResult, monCal.multiplier());
+    }
 
 	/**
 	 * Test method for {@link calculs.Calculs#additionner()}.
 	 */
-	@Test
-	void testAdditionner() {
-		if(c1.additionner()!=3) {
-		fail("Not yet implemented");
-	}
-	}
-
+	@ParameterizedTest(name="Addition numéro {index}: nombre1={0}, nombre2={1}, résultat attendu = {2}")
+    @MethodSource("chargerJeuDeTestAdd")
+    void testAdditionner(int firstNumber, int secondNumber, int expectedResult) {
+            Calculs monCal = new Calculs(firstNumber, secondNumber);
+            assertEquals(expectedResult, monCal.additionner());
+    }
 	/**
 	 * Test method for {@link calculs.Calculs#diviser()}.
 	 */
 	
-	@Test
-	void testDiviser() {
-		if (c1.diviser()!=0) {
-		fail("Not yet implemented");
-		}
+	@ParameterizedTest(name="Division numéro {index}: nombre1={0}, nombre2={1}, résultat attendu = {2}")
+    @MethodSource("chargerJeuDeTestDiv")
+    void testDiviser(int firstNumber, int secondNumber, int expectedResult) {
+            Calculs monCal = new Calculs(firstNumber, secondNumber);
+            assertEquals(expectedResult, monCal.diviser());
 	}
 
 	/**
 	 * Test method for {@link calculs.Calculs#soustraire()}.
 	 */
-	@Test
-	void testSoustraire() {
-		if (c1.soustraire()!=-1) {
-		fail("Not yet implemented");
-		}
-	}
+	@ParameterizedTest(name="Soustraction numéro {index}: nombre1={0}, nombre2={1}, résultat attendu = {2}")
+    @MethodSource("chargerJeuDeTestSou")
+    void testSoustraire(int firstNumber, int secondNumber, int expectedResult) {
+            Calculs monCal = new Calculs(firstNumber, secondNumber);
+            assertEquals(expectedResult, monCal.soustraire());
+    }
 
 }
